fd4ca46ab307bb60ae766a9fd860f1baaa1c3ba5
js340 <jaskaransingh3433@gmail.com>
2022-03-10 20:45:34 +0000

    new project created and replaced because of git error

--- Crypto-Alerts/App.js
+++ Crypto-Alerts/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigation from "./src/navigation";
-import WatchlistProvider from "./src/contexts/watchlistContext";
+import WatchlistProvider from "./src/contexts/watchListContext";
 
 export default function App() {
   return (
